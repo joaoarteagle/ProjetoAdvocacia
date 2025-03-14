4b1032cdbdcc1114364cf3733856fc40e312467b
JoaoAndrade <joaopaulo.arteagle17@gmail.com>
2025-03-14 13:24:26 -0300

    testando credenciais e escopo

--- credentials.py
+++ credentials.py
@@ -1,5 +1,8 @@
 import os
+import json
+from google.oauth2.service_account import Credentials
 
-SERVICE_ACCONT_FILE = os.getenv('SERVICE_ACCONT_FILE')
+SERVICE_ACCONT_FILE = Credentials.from_service_account_info(json.loads(os.getenv('SERVICE_ACCONT_FILE')))
 
-SCOPES = os.getenv('SCOPES')
+SCOPES = json.loads(os.getenv('SCOPES'))
+
