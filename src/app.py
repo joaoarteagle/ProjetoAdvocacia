340e72e4e563f5e295486b991fa3ec7a3eb21b5d
JoaoAndrade <joaopaulo.arteagle17@gmail.com>
2025-03-18 15:42:53 -0300

    realizando testes no deploy

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import threading
 from src.main import main
+import os
 
 
 
@@ -22,4 +23,5 @@ def webhook():
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.env.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
