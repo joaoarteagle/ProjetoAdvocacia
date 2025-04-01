7353d5fbfa9da3a3134d5e8b2494e23baedf4b03
JoaoAndrade <joaopaulo.arteagle17@gmail.com>
2025-04-01 17:32:29 -0300

    alterando captura de variaveis de ambiente

--- src/credentials.py
+++ src/credentials.py
@@ -12,7 +12,19 @@ print(f"Variável capturada: {json_str[:50]}...")  # Exibe apenas parte do JSON 
 # Agora tente carregar o JSON corretamente
 SERVICE_ACCONT_FILE = json.loads(json_str)  # Usa json.loads() para strings JSON
 
-SCOPES = json.load(os.environ.get('SCOPES'))
+
+
+
+
+json_str = os.environ.get('SCOPES')
+
+if json_str is None:
+    raise ValueError("Erro: A variável de ambiente 'SCOPES' não foi encontrada!")
+
+print(f"Variável capturada: {json_str[:50]}...")  # Exibe apenas parte do JSON para verificar
+
+# Agora tente carregar o JSON corretamente
+SCOPES = json.load(json_str)
 
 # def json_to_string(file_path):
 #     with open(file_path) as file:
